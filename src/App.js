ce9d0d6de3138eecfd001af33b13a07a3fbd5969
msr1013 <rawalsinghmanish@gmail.com>
2021-01-23 10:33:51 +0545

    header_component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,7 @@ function App() {
   return (
     <div>
 
-      <div className = "header ">
-          <h1 className="main_title">SIMPLE CSS TEMPLATE</h1>
-          <h5 className="subtitle">WELCOME TO OUR CHURCH</h5>
-          <button className = "btn">Join With Us</button>
-
-      </div>
+      
 
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className = "header ">
+          <h1 className="main_title">SIMPLE CSS TEMPLATE</h1>
+          <h5 className="subtitle">WELCOME TO OUR CHURCH</h5>
+          <button className = "btn">Join With Us</button>
+
+      </div>
+    )
+}
