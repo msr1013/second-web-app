e796cf271870fdb9cc42be4b480cd88a10a97873
msr1013 <rawalsinghmanish@gmail.com>
2021-01-23 10:05:43 +0545

    'react_frontend_webapp'

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+
+      <div className = "header ">
+          <h1 className="main_title">SIMPLE CSS TEMPLATE</h1>
+          <h5 className="subtitle">WELCOME TO OUR CHURCH</h5>
+          <button className = "btn">Join With Us</button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
