711e14c989500c5af5147cba594c2955784b312b
msr1013 <rawalsinghmanish@gmail.com>
2021-01-27 16:43:09 +0545

    contact-form-component

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -3,25 +3,57 @@ import './contact.css'
 import Button from '../../widgets/button'
 class Form extends Component {
     state = {
-        name: ''
+        name: '',
+        email: '',
+        phone: '',
+        message: ''
     }
 
-    handleNameChange = (e) => {
+    // handleNameChange = (e) => {
+    //     this.setState({
+    //         name: e.target.value
+    //     })
+    // }
 
+    // handleEmailChange = (e) => {
+    //     this.setState({
+    //         email: e.target.value
+    //     })
+    // }
+    // handlePhoneChange = (e) => {
+    //     this.setState({
+    //         phone: e.target.value
+    //     })
+    // }
+
+    // handleMessageChange = (e) => {
+    //     this.setState({
+    //         message: e.target.value
+    //     })
+    // }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
+    handleFormSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+    }
     render() {
 
         return (
             <div id="contact-form">
-                <h2>{this.state.name}</h2>
+
                 <h1>Contact Us</h1>
-                <form>
-                    <input type="text" name="name" placeholder="Name" onChange={this.handleNameChange} />
-                    <input type="text" name="email" placeholder="Email" />
-                    <input type="text" name="phone" placeholder="Phone" />
-                    <textarea name="message" placeholder="Message"></textarea>
-                    <Button title="Submit"></Button>
+                <form onSubmit={this.handleFormSubmit}>
+                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+                    <input type="text" name="email" placeholder="Email" onChange={this.handleChange} />
+                    <input type="text" name="phone" placeholder="Phone" onChange={this.handleChange} />
+                    <textarea name="message" placeholder="Message" onChange={this.handleChange}></textarea>
+                    <Button type="submit" title="Submit" />
                 </form>
 
             </div>
