7185ad512f5ce11ad7f882b3a90f5c12ebf69ce0
msr1013 <rawalsinghmanish@gmail.com>
2021-01-28 15:40:34 +0545

    custom-validation-contact-form

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -6,7 +6,8 @@ class Form extends Component {
         name: '',
         email: '',
         phone: '',
-        message: ''
+        message: '',
+        errors: {}
     }
 
     // handleNameChange = (e) => {
@@ -34,25 +35,57 @@ class Form extends Component {
 
     handleChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
+            errors: {
+                ...this.state.errors,
+                [e.target.name]: ''
+            }
         })
     }
 
     handleFormSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state)
+        let errors = {}
+
+        let isValid = true
+
+        if (this.state.name === '') {
+            errors.name = "Name is required"
+            isValid = false
+        } else if (this.state.name.length < 5) {
+            errors.name = "Name should be atleast 5 char"
+            isValid = false
+        }
+        if (this.state.message === '') {
+            errors.message = "Message is required"
+            isValid = false
+        } else if (this.state.message.length < 10) {
+            errors.message = "Message should be atleast 10 char"
+            isValid = false
+        }
+
+        this.setState({
+            errors: errors
+        })
+
+        if (isValid) {
+            console.log("the form is valid ")
+        }
+
     }
     render() {
-
+        const { name, email, phone, message } = this.state
         return (
             <div id="contact-form">
 
                 <h1>Contact Us</h1>
                 <form onSubmit={this.handleFormSubmit}>
-                    <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
-                    <input type="text" name="email" placeholder="Email" onChange={this.handleChange} />
-                    <input type="text" name="phone" placeholder="Phone" onChange={this.handleChange} />
-                    <textarea name="message" placeholder="Message" onChange={this.handleChange}></textarea>
+                    <input type="text" className={this.state.errors.name ? 'error-block' : ''} name="name" value={name} placeholder="Name" onChange={this.handleChange} />
+                    <div className="error">{this.state.errors.name}</div>
+                    <input type="text" name="email" value={email} placeholder="Email" onChange={this.handleChange} />
+                    <input type="text" name="phone" value={phone} placeholder="Phone" onChange={this.handleChange} />
+                    <textarea name="message" className={this.state.errors.message ? 'error-block' : ''} placeholder="Message" value={message} onChange={this.handleChange}></textarea>
+                    <div className="error">{this.state.errors.message}</div>
                     <Button type="submit" title="Submit" />
                 </form>
 
